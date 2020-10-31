6ef9ee1a682cfd484a089daeac629eda32acaa68
Alex <alex@dgmc.co.uk>
2020-10-31 08:58:33 +0200

    add: project description blocks

---
+++ page_parts/village-plan-text.php
@@ -0,0 +1,44 @@
+<div class="description">
+    <div class="pricing">
+        <div class="title">Вартість без ремонтних робіт та матеріалів</div>
+        <table cellspacing="0" cellpadding="0" border-collapsse>
+            <thead>
+            <tr>
+                <td></td>
+                <td>Оплата 100%</td>
+                <td>Розсрочка 2 роки, перший внесок 30%</td>
+                <td>Розсрочка 3 роки, перший внесок 20%</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1 черга: вересень 2020 - лютий 2023</td>
+                <td>17000грн за кв.м.</td>
+                <td>18000грн за кв.м.</td>
+                <td>19000грн за кв.м.</td>
+            </tr>
+            <tr>
+                <td>2 черга: вересень 2020 - лютий 2023</td>
+                <td>18000грн за кв.м.</td>
+                <td>19000грн за кв.м.</td>
+                <td>20000грн за кв.м.</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="renovation">
+        <i class="fa fa-info-circle"></i> Вартість ремонту 140 у.о. кв.м.
+    </div>
+
+    <div class="zones">
+        <div class="title">Зонавання таунхаус Смарагд</div>
+        <ul>
+            <li>1. Паркінг, 20 кв.м.</li>
+            <li>2. Столова зона, 16 кв.м.</li>
+            <li>3. Зелена зона, 69 кв.м.</li>
+            <li>4. Барбекю, 4 кв.м.</li>
+            <li>5. Альтанки, 9 кв.м.</li>
+        </ul>
+    </div>
+</div>
