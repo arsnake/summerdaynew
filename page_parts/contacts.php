b1aa7aa0b76e44f9b1d5664f3cf83acf2b48156b
Alex <alex@dgmc.co.uk>
2020-11-19 06:22:54 +0200

    add ga classes

--- page_parts/contacts.php
+++ page_parts/contacts.php
@@ -1,7 +1,7 @@
 <div class="contacts">
-    <a href="tel:0977942020"><i class="fa fa-phone"></i>097 794-20-20</a>
-    <a href="tel:0977941919"><i class="fa fa-phone"></i>097 794-19-19</a>
-    <a href="mailto:sales@summer.day.ua"><i class="fa fa-inbox"></i>sales@summer.day.ua</a>
-    <a href="https://goo.gl/maps/VVFCYp1ZUMCj7Utw7" target="_blank"><i class="fa fa-map"></i>Україна, Запорізька обл.,<br>м. Приморськ, вул. Курортна, 25</a>
-    <a href="https://goo.gl/maps/ZAu4qx5xHchu45Bi9" target="_blank"><i class="fa fa-map"></i>Україна, Запорізька обл.,<br>м. Приморськ, вул. Курортна, 93</a>
+    <a href="tel:0977942020" class="ga_click_contact_0977942020"><i class="fa fa-phone"></i>097 794-20-20</a>
+    <a href="tel:0977941919" class="ga_click_contact_0977941919"><i class="fa fa-phone"></i>097 794-19-19</a>
+    <a href="mailto:sales@summer.day.ua" class="ga_click_contact_email"><i class="fa fa-inbox"></i>sales@summer.day.ua</a>
+    <a href="https://goo.gl/maps/VVFCYp1ZUMCj7Utw7"  class="ga_click_map_apartments"  target="_blank"><i class="fa fa-map"></i>Україна, Запорізька обл.,<br>м. Приморськ, вул. Курортна, 25</a>
+    <a href="https://goo.gl/maps/ZAu4qx5xHchu45Bi9"  class="ga_click_map_village" target="_blank"><i class="fa fa-map"></i>Україна, Запорізька обл.,<br>м. Приморськ, вул. Курортна, 93</a>
 </div>
--- page_parts/map.php
+++ page_parts/map.php
@@ -5,12 +5,12 @@
             <div class="google-link apartments">
                 <div class="title"><i class="fa fa-map-marker-alt"></i>Grand Family Apartments</div>
                 <div class="address">Україна, Запорізька обл., м. Приморськ, вул. Курортна, 25</div>
-                <a href="https://goo.gl/maps/VVFCYp1ZUMCj7Utw7" target="_blank">Відкрити в Google Maps<i class="fa fa-map-marked-alt"></i></a>
+                <a href="https://goo.gl/maps/VVFCYp1ZUMCj7Utw7" target="_blank" class="ga_click_map_apartments">Відкрити в Google Maps<i class="fa fa-map-marked-alt"></i></a>
             </div>
             <div class="google-link village">
                 <div class="title"><i class="fa fa-map-marker-alt"></i>Grand Family Village</div>
                 <div class="address">Україна, Запорізька обл., м. Приморськ, вул. Курортна, 93</div>
-                <a href="https://goo.gl/maps/ZAu4qx5xHchu45Bi9" target="_blank">Відкрити в Google Maps<i class="fa fa-map-marked-alt"></i></a>
+                <a href="https://goo.gl/maps/ZAu4qx5xHchu45Bi9" target="_blank" class="ga_click_map_village">Відкрити в Google Maps<i class="fa fa-map-marked-alt"></i></a>
             </div>
         </div>
     </div>
