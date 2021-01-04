9a716df7b0991d37e92b00babd151a60f5f1cd3b
Alex <alex@dgmc.co.uk>
2021-01-04 08:55:20 +0200

    ny

---
+++ news-5.php
@@ -0,0 +1,36 @@
+<?php include 'page_parts/header.php'; ?>
+
+    <div class="row">
+        <div class="inrow">
+            <div class="h2">Новий рік</div>
+            <div class="crumbs">
+                <ul>
+                    <li><a class="item" href="/">Головна /</a></li>
+                    <li><a class="item" href="/news">Новини /</a></li>
+                    <li><span class="item">Новий рік</span></li>
+                </ul>
+            </div>
+
+            <div class="article-content">
+                <div class="col-50">
+                    <div class="date"><i class="fa fa-calendar-alt"></i>31 грудня 2020</div>
+                    <div class="text">
+                        <p>Дорогі друзі!!!</p>
+                        <p>Щиро вітаємо Вас із Новим Роком та Різдвом Христовим.</p>
+                        <p>2020 рік був дійсно викликом для усіх нас, але ми із гідністю пережили його і входимо у новий рік із зміцненням позицій, незважаючи на усі виклики.</p>
+                        <p>Бажаємо вам у 2021 році більше позитивних вражень, натхнень та нових перемог!</p>
+                        <p>Нехай прийдешній рік принесе вам  достатку, та надміцного імунітету від усіх негараздів. А ми й надалі будемо наполегливо працювати, щоб принести у ваші оселі та офіси затишок та упевненість у тому, що усе буде гаразд.</p>
+                        <p>Ваша команда Isatex Invest Group</p>
+                    </div>
+                </div>
+                <div class="col-50"><img src="assets/images/news/news-4.jpg" alt="Оновлення сайту"></div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+<?php include 'page_parts/buy_rent.php'; ?>
+<?php include 'page_parts/last_news.php'; ?>
+<?php include 'page_parts/footer.php'; ?>
